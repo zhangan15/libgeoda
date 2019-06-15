b32ebb6e9433b34ea80cb87757e0e731e2db92ab
Xun Li <lixun910@gmail.com>
2019-06-14 22:07:29 -0700

    update for building on Ubuntu

--- swig/R/build.sh
+++ swig/R/build.sh
@@ -7,17 +7,19 @@ swig -c++ -r -I../../deps/libgeoda/include -o libgeoda.cpp ../libgeoda.i
 rm -rf build
 mkdir build
 cp -rf pkg-template build/libgeoda
+
+mkdir build/libgeoda/R
+mkdir build/libgeoda/docs
+cp -rf ../../deps/ build/libgeoda/src/
+
 cp libgeoda.R build/libgeoda/R/
 cp sf_geoda.R build/libgeoda/R/
 cp libgeoda.cpp build/libgeoda/src
 cp *.Rmd build/libgeoda/docs
 cp *.html build/libgeoda/docs
-mkdir build/libgeoda/src/deps
-mkdir build/libgeoda/src/deps/libgeoda
-cp -rf ../../deps/ build/libgeoda/src/deps/
 cd build
 
-R CMD BUILD libgeoda
+R CMD build libgeoda
 R CMD REMOVE libgeoda
 R CMD INSTALL libgeoda_0.1.0.tar.gz
 
