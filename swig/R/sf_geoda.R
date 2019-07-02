ce3bb651783600856eeaace587ec5c202c7265c1
Xun Li <xunli@uchicago.edu>
2019-07-02 15:54:59 +0800

    add sf_to_geoda function

--- swig/R/sf_geoda.R
+++ swig/R/sf_geoda.R
@@ -3,31 +3,69 @@ random_string <- function(n = 5000) {
   paste0(a, sprintf("%04d", sample(9999, n, TRUE)), sample(LETTERS, n, TRUE))
 }
 
-sf_to_geoda = function(sf_obj) {
-  if (!require("sf")) {
-    stop("package sf not available: install first?")
-  }
-}
-
 geoda_to_sf = function(gda) {
   if (!require("sf")) {
     stop("package sf not available: install first?")
   }
 }
 
-open_geoda = function(path) {
-  dll_path <- system.file("lib/libgeoda.dll", package="libgeoda")
-  src_path <- system.file("lib/libgeoda.R", package="libgeoda")
-  old_dir <- getwd()
-  new_dir <- dirname(dll_path)
-  setwd(new_dir)
-  print("load libgeoda.dll")
-  print(new_dir)
-  dyn.load("libgeoda.dll")
-  source("libgeoda.R")
-  cacheMetaData(1)
-  setwd(old_dir)
-  return (GeoDa(path))
+
+sf_to_geoda = function(sf_obj) {
+  if (!require("sf")) {
+    stop("package sf not available: install first?")
+  }
+  if (!require("wkb")) {
+    stop("package wkb not available: install first?")
+  }
+  
+  # geometries
+  sf_geom <- st_geometry(sf_obj)
+  geoms_wkb <- st_as_binary(sf_geom)
+  n_obs <- length(sf_geom)
+  wkb_bytes_len <- sapply(geoms_wkb, function(x){ return(length(x))})
+  wkb_vec <- unlist(geoms_wkb)
+  
+  # in-memory name
+  file_name <- random_string(1)
+  
+  # table
+  sf_df <- as.data.frame(sf_obj)
+  col_names <- colnames(sf_df)
+  n_cols <- length(col_names)
+  tbl <- GeoDaTable()
+  for (i in 1:n_cols) {
+    col_nm <- col_names[[i]]
+    if (col_nm == "geometry") next
+
+    dat <- sf_df[, col_nm]
+    ft <- class(dat)
+    if (ft == "factor") {
+      tbl$AddStringColumn(col_nm, dat)
+      
+    } else if (ft == "integer" || ft == "logical") {
+      tbl$AddIntColumn(col_nm, dat)
+      
+    } else if (ft == "double" || ft == "numeric") {
+      tbl$AddRealColumn(col_nm, dat)
+      
+    } else {
+      dat <- as.character(dat)
+      tbl$AddStringColumn(col_names[[i]], dat)
+    }
+  }
+  # map_type
+  map_type <- "map_polygons"
+  geom_type <- st_geometry_type(sf_obj)[[1]]
+  if (geom_type == "MULTIPOINT" || geom_type == "POINT") {
+    map_type <- "map_points"
+  } else if (geom_type == "MULTILINESTRING" || geom_type == "LINESTRING") {
+    map_type <- "map_lines"
+  }
+
+  # prj4
+  proj4_str <- st_crs(sf_obj)[[2]]
+  gda <- GeoDa(file_name, map_type, n_obs, tbl, as.integer(wkb_vec), wkb_bytes_len, proj4_str)
+  return(gda)
 }
 
 # create a GeoDa object from a sp object
@@ -39,7 +77,7 @@ sp_to_geoda = function(sp_obj) {
     stop("package wkb not available: install first?")
   }
   # geometries
-  geoms_wkb = writeWKB(sp_obj)
+  geoms_wkb <- writeWKB(sp_obj)
   n_obs <- length(geoms_wkb)
   wkb_bytes_len <- sapply(geoms_wkb, function(x) {return(length(x))})
   wkb_vec <- unlist(geoms_wkb)
--- swig/R/test.R
+++ swig/R/test.R
@@ -3,6 +3,16 @@ source("rgeoda.R")
 cacheMetaData(1)
 
 source("sf_geoda.R")
-gda <- GeoDa("../../data/natregimes.shp")
+#gda <- GeoDa("../../data/natregimes.shp")
+#sp_nat <- geoda_to_sp(gda)
 
-sp_nat <- geoda_to_sp(gda)
+
+library(sf)
+nc <- st_read("../../data/natregimes.shp")
+
+gda <- sf_to_geoda(nc)
+data <- gda$GetNumericCol("HR60")
+queen_w <- gda$CreateContiguityWeights()
+lisa <- gda$LISA(queen_w, data)
+lags <- lisa$GetLagValues()
+lags[1:10]
